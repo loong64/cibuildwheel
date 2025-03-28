10ca6e245692c0d702f50c6a5d135df60207e09e
吴小白 <296015668@qq.com>
2025-03-28 15:01:59 +0800

    feat: add loongarch64 platform support
    Signed-off-by: 吴小白 <296015668@qq.com>

--- unit_test/oci_container_test.py
+++ unit_test/oci_container_test.py
@@ -37,6 +37,7 @@ DEFAULT_OCI_PLATFORM = {
     "s390x": OCIPlatform.S390X,
     "aarch64": OCIPlatform.ARM64,
     "arm64": OCIPlatform.ARM64,
+    "loongarch64": OCIPlatform.LOONG64,
 }[pm]
 
 PODMAN = OCIContainerEngineConfig(name="podman")
@@ -589,6 +590,7 @@ def test_multiarch_image(container_engine, platform):
             OCIPlatform.AMD64: ("x86_64",),
             OCIPlatform.ARMV7: ("armv7l", "armv8l"),
             OCIPlatform.ARM64: ("aarch64",),
+            OCIPlatform.LOONG64: ("loongarch64",),
             OCIPlatform.PPC64LE: ("ppc64le",),
             OCIPlatform.S390X: ("s390x",),
         }
@@ -599,6 +601,7 @@ def test_multiarch_image(container_engine, platform):
             OCIPlatform.AMD64: "amd64",
             OCIPlatform.ARMV7: "armhf",
             OCIPlatform.ARM64: "arm64",
+            OCIPlatform.LOONG64: "loong64",
             OCIPlatform.PPC64LE: "ppc64el",
             OCIPlatform.S390X: "s390x",
         }
