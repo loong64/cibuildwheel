4f2b114d7449425f2d42b2e0779836c5991b5c5c
吴小白 <296015668@qq.com>
2025-07-09 08:09:06 +0800

    feat: add loongarch64 platform support
    Signed-off-by: 吴小白 <296015668@qq.com>

--- bin/generate_schema.py
+++ bin/generate_schema.py
@@ -154,6 +154,9 @@ properties:
   manylinux-i686-image:
     type: string
     description: Specify alternative manylinux / musllinux container images
+  manylinux-loongarch64-image:
+    type: string
+    description: Specify alternative manylinux / musllinux container images
   manylinux-ppc64le-image:
     type: string
     description: Specify alternative manylinux / musllinux container images
@@ -182,6 +185,9 @@ properties:
     type: string
     description: Specify alternative manylinux / musllinux container images
   musllinux-i686-image:
+    type: string
+    description: Specify alternative manylinux / musllinux container images
+  musllinux-loongarch64-image:
     type: string
     description: Specify alternative manylinux / musllinux container images
   musllinux-ppc64le-image:
